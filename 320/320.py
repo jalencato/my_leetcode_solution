35b657580a19db7583fb99ece441085b833a9af4
jalencato <jalencato23pistons@gmail.com>
2020-08-17 17:57:33 +0800

    Medium 320

--- 320/320.py
+++ 320/320.py
@@ -7,18 +7,13 @@ class Solution:
 
         def helper(curr_pos, s, cnt):
             if curr_pos == len(word):
-                if cnt > 0:
-                    res.append(s + str(cnt))
-                else:
-                    res.append(s)
+                res.append(s + str(cnt)) if cnt > 0 else res.append(s)
                 return
+
             # number caller
             helper(curr_pos + 1, s, cnt + 1)
             # string caller
-            if cnt > 0:
-                helper(curr_pos + 1, s + str(cnt) + word[curr_pos], 0)
-            else:
-                helper(curr_pos + 1, s + word[curr_pos], 0)
+            helper(curr_pos + 1, s + str(cnt) + word[curr_pos], 0) if cnt > 0 else helper(curr_pos + 1, s + word[curr_pos], 0)
         helper(0, "", 0)
         return res
 
