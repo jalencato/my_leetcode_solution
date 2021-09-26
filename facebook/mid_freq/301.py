5996f3133d619d727652dc0a66c2dc2fac5395d7
cyrus <jalencato23pistons@gmail.com>
2021-09-26 16:37:09 -0700

    min_freq

---
+++ facebook/mid_freq/301.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        
+
+if __name__ == '__main__':
+    sol = Solution()
+    st = '()())()'
+    print(sol.removeInvalidParentheses(st))
