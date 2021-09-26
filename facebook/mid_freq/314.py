ba3cec319ef7d906b6ede4df51188128876e0408
cyrus <jalencato23pistons@gmail.com>
2021-09-26 16:45:22 -0700

    min_freq

--- facebook/mid_freq/314.py
+++ facebook/mid_freq/314.py
@@ -4,5 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+import collections
+
+
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        cols = collections.defaultdict(list)
+        queue = [(root, 0)]
+        for node, i in queue:
+            if node:
+                cols[i].append(node.val)
+                queue.append((node.left, i-1))
+                queue.append((node.right, i+1))
+        return [cols[i] for i in sorted(cols)]
+
