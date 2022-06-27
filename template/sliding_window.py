b5dc98708a6f378c1ea69edcbbd4db5ff44eb163
Cyrus <cyrus7lakers@gmail.com>
2022-06-26 20:31:21 -0700

    Summer 2022

---
+++ template/sliding_window.py
@@ -0,0 +1,24 @@
+import heapq
+
+
+class Solution:
+    def max_sliding_window(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        if n == 0:
+            return []
+        q = collections.deque()
+        for i in range(k):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+
+        ans = [nums[q[0]]]
+        for i in range(k, n):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+            while q[0] <= i - k:
+                q.popleft()
+            ans.append(nums[q[0]])
+
+        return ans
